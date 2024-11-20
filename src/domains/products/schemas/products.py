b04ea0423fec340760e2bde252da5b9eafe07048
AniutaP <musenok.ko@mail.ru>
2024-11-20 21:40:28 +0300

    fix schema

--- src/domains/products/schemas/products.py
+++ src/domains/products/schemas/products.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel, ConfigDict, Field
+from decimal import Decimal
 
 
 class ProductAddSchema(BaseModel):
     name: str
-    description: str | None = Field(default=None)
-    price: int = Field(ge=0)
+    description: str | None = Field(default=None, max_length=150)
+    price: Decimal = Field(ge=0)
     quantity: int = Field(ge=0)
 
     model_config = ConfigDict(from_attributes=True)
